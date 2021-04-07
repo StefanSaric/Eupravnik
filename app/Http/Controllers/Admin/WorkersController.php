f9258df2953dd2e87b2f994f1abc6c829011f4d3
ceric <stefan.ceric.exe4u@gmail.com>
2021-04-07 18:44:22 +0200

    fixed Worker and WorkersType models,migrations;upgraded WorkersController; upgraded allworkers view and fixed data table;

--- app/Http/Controllers/Admin/WorkersController.php
+++ app/Http/Controllers/Admin/WorkersController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Worker_Types;
-use App\Workers;
+use App\WorkersType;
+use App\Worker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
@@ -13,17 +13,29 @@ class WorkersController extends Controller
 {
 
     public function index () {
-        $workers = Workers::all();
+        
+        $workers = Worker::join('workers_types', 'workers_types.id', '=', 'workers.type_id')
+                ->select('workers.id as id',
+                        'workers.first_name as name',
+                        'workers.last_name as surname',
+                        'workers_types.name as worker_type',
+                        'workers.email as email',
+                        'workers.telephone as telephone',
+                        'workers.licence as licence')
+                ->get();
+        
+        //dd($workers);
+        //$workers = Worker::all();
         return view('admin.workers.allworkers', ['active' => 'allWorkers', 'workers' => $workers]);
     }
 
     public function create (){
-        $types = Worker_Types::all();
+        $types = WorkersType::all();
         return view('admin.workers.create', ['active' => 'addWorker', 'types' => $types]);
     }
 
     public function store (Request $request) {
-        $worker = Workers::create(['email' => $request->email,
+        $worker = Worker::create(['email' => $request->email,
             'password' => $request->password,
             'password_confirm' => $request->password_confirm,
             'type_id' => $request->type_id,
@@ -38,8 +50,8 @@ class WorkersController extends Controller
 
     public function edit($id)
     {
-        $worker = Workers::find($id);
-        $types = Worker_Types::all();
+        $worker = Worker::find($id);
+        $types = WorkersType::all();
 
         return view ('admin.workers.edit', ['active' => 'addWorker', 'worker' => $worker, 'types' => $types ]);
     }
@@ -47,7 +59,7 @@ class WorkersController extends Controller
     public function update(Request $request)
     {
         $id = $request->worker_id;
-        $worker = Workers::find($id);
+        $worker = Worker::find($id);
         $worker->update($request->except(['password']));
         if(isset($request->password) && $request->password != ''){
             $worker->password = bcrypt($request->input('password'));
