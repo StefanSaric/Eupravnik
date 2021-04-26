c434bd43263a392d300fd75f030629120bfd1bd7
stefan <stefan.saric.exe4u@gmail.com>
2021-04-26 13:47:03 +0200

    -- resize document table for mobile view

--- resources/views/admin/documents/alldocuments.blade.php
+++ resources/views/admin/documents/alldocuments.blade.php
@@ -73,7 +73,7 @@
                                                     @foreach($documents as $num => $document)
                                                         <tr id="{{ $document->id }}" class="gradeX">
                                                             <td>&nbsp;&nbsp;&nbsp;{{ $num + 1 }}</td>
-                                                            <td><a href="{{url('/'.$document->url)}}" target="_blank">{{ $document->name }}</a></td>
+                                                            <td class="show-on-small show-on-medium"><a href="{{url('/'.$document->url)}}" target="_blank">{{ $document->name }}</a></td>
                                                             <td>{{ $document->created_at }}</td>
                                                             <td>{{ $document->type }}</td>
                                                             <td style='white-space: nowrap; vertical-align: middle'>
