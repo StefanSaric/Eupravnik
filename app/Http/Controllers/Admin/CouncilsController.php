5c276c0c6136492a27014709cca3df67e1a972b5
stefan <stefan.saric.exe4u@gmail.com>
2021-06-30 15:30:20 +0200

    -- fixing error for councils stewards

--- app/Http/Controllers/Admin/CouncilsController.php
+++ app/Http/Controllers/Admin/CouncilsController.php
@@ -109,7 +109,10 @@ class CouncilsController extends Controller
      */
     public function create ()
     {
-        $stewards = Steward::where('firm_id', '=' ,Auth::user()->id);
+        $stewards = Steward::where('firm_id', '=' ,Auth::user()->id)
+                    ->join('users', 'users.id', '=', 'stewards.id')
+                    ->select( 'stewards.id as id', 'users.name as name')
+                    ->get();
 
         return view('admin.councils.create', ['active' => 'addCouncil', 'stewards' => $stewards]);
     }
